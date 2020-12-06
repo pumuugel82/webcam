b875f046384211e71e54964ecfe4bb888ad1cfbc
Christoph Hirte <christoph.hirte@gmail.com>
2020-12-06 22:51:03 +0100

    View latest picture
    automatically shows latest snapshot on frontpage

--- index.js
+++ index.js
@@ -10,7 +10,9 @@
 
     app.get("/", (req, res) => {res.sendFile(__dirname + "/index.html")});
     app.get("/index.html", (req, res) => {res.sendFile(__dirname + "/index.html")});
-    app.get("/api/picture/all", pictures);
+    app.get("/jquery-3.5.1.min.js", (req, res) => {res.sendFile(__dirname + "/jquery-3.5.1.min.js")});
+    app.get("/api/pictures/all", all);
+    app.get("/api/pictures/latest", latest);
     app.get("/api/picture/:name", picture);
 
     app.listen(port, () => {
@@ -22,7 +24,7 @@
      * @param {*} req 
      * @param {*} res 
      */
-    function pictures(req, res) {
+    function all(req, res) {
         let filesPromise = fs.promises.readdir(picSrcDir);
         filesPromise.then((filesArray) => {
             res.send(JSON.stringify(filesArray));            
@@ -47,6 +49,31 @@
               log(err)
             }
           });
+    }
+
+    /**
+     * Liefert das älteste Bild zurück.
+     * @param {*} req 
+     * @param {*} res 
+     */
+    function latest(req, res) {
+
+        let filesPromise = fs.promises.readdir(picSrcDir);
+        
+        filesPromise.then((filesArray) => {
+            // Vieleicht zu umstaendlich aber es geht erstmal...
+            let ary = filesArray.sort().reverse();
+
+            // lieber path.join()...
+            let fullPicPath = picSrcDir + "/" + ary[0];
+            log(fullPicPath);
+            
+            res.sendFile(fullPicPath, {}, function (err) {
+                if (err) {
+                    log(err)
+                }
+            });
+        });
     }
 
     function log(what) {
